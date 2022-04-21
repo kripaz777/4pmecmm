fc4318b29df7265ddef71ef5c8e89cdc21098934
aayusha-aigeal <aayush.adhikari@aigeal.com>
2022-04-21 17:10:19 +0545

    home page

--- home/views.py
+++ home/views.py
@@ -14,6 +14,9 @@ class HomeView(BaseView):
 		self.views['sliders'] = Slider.objects.all()
 		self.views['ads'] = Ad.objects.all()
 		self.views['products'] = Product.objects.filter(stock = 'In Stock')
+		self.views['sale_products'] = Product.objects.filter(labels = 'sale', stock = 'In Stock')
+		self.views['hot_products'] = Product.objects.filter(labels = 'hot',stock = 'In Stock')
+		self.views['new_products'] = Product.objects.filter(labls = 'new' , stock = 'In Stock')
 
 		return render(request,'shop-index.html',self.views)
 
